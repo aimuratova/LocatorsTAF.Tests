cff05457c3d0a96a6b5ec391f70f6cc68da02c64
Aigerim Muratova <ai.muratova@mail.ru>
2026-07-27 10:56:27 +0500

    Added changes into main page

--- LocatorsTAF.BusinessLayer/Pages/MainPage.cs
+++ LocatorsTAF.BusinessLayer/Pages/MainPage.cs
@@ -1,6 +1,9 @@
 ﻿using LocatorsTAF.CoreLayer.Element;
 using LocatorsTAF.CoreLayer.Interfaces;
+using LocatorsTAF.CoreLayer.Utilities;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +20,9 @@ namespace LocatorsTAF.BusinessLayer.Pages
         private readonly IWebElementWrapper findButton;
         private readonly IWebElementWrapper pdfDownloadLink;
         private readonly IWebElementWrapper insightLink;
+        private readonly IWebElementWrapper servicesLink;
+
+        private WebDriverWait wait;
 
         public MainPage(IWebDriverWrapper driver) : base(driver)
         {
@@ -26,8 +32,12 @@ namespace LocatorsTAF.BusinessLayer.Pages
             findButton = new WebElementWrapper(driver, By.XPath("//button[.//span[normalize-space()='Find']]"));
             pdfDownloadLink = new WebElementWrapper(driver, By.XPath("//a[contains(@href,'Code-Of-Conduct_01_26.pdf')]"));
             insightLink = new WebElementWrapper(driver, By.CssSelector("a.top-navigation__item-link[href='/insights']"));
+            servicesLink = new WebElementWrapper(driver, By.XPath("//a[@href='/services']"));
+
+            wait = new WebDriverWait(driver.GetWebDriver(), TimeSpan.FromSeconds(ConfigurationService.ImplicitWaitTime));
+
         }
-        
+
         public InsightsPage NavigateToInsightsPage()
         {
             insightLink.Click();
@@ -58,6 +68,22 @@ namespace LocatorsTAF.BusinessLayer.Pages
             var pdf = pdfDownloadLink.FindElement();
             ((IJavaScriptExecutor)driver.GetWebDriver()).ExecuteScript("arguments[0].scrollIntoView({block: 'center'});", pdf);
             pdf.Click();
+        }
+
+        public ServicesPage NavigateToServices(string searchServiceText)
+        {
+            var services = wait.Until(d=> d.FindElement(By.XPath("//li[contains(@class,'top-navigation__item')][.//a[@href='/services']]")));
+
+            new Actions(driver.GetWebDriver())
+                .MoveToElement(services)
+                .Pause(TimeSpan.FromMilliseconds(500))
+                .Perform();
+
+            var service = wait.Until(d => d.FindElement(By.XPath($"//div[contains(@class,'top-navigation__flyout')]//a[normalize-space()='{searchServiceText}']")));
+
+            service.Click();
+
+            return new ServicesPage(driver);
         }
     }
 }
---
+++ LocatorsTAF.BusinessLayer/Pages/ServicesPage.cs
@@ -0,0 +1,31 @@
+﻿using LocatorsTAF.CoreLayer.Element;
+using LocatorsTAF.CoreLayer.Interfaces;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocatorsTAF.BusinessLayer.Pages
+{
+    public class ServicesPage : BasePage
+    {
+        private readonly IWebElementWrapper pageContent;
+
+        public ServicesPage(IWebDriverWrapper webDriverWrapper) : base(webDriverWrapper)
+        {
+            pageContent = new WebElementWrapper(webDriverWrapper, By.Id("main"));
+        }
+
+        public string GetPageTitleInService()
+        {
+            return pageContent.GetText();
+        }
+
+        public bool IsServicesSearchStringIsFound()
+        {
+            return pageContent.GetText().Contains("Our Related Expertise", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
