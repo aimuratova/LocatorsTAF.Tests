36e36bcba538956217821b511b09cc5bd3954946
Aigerim Muratova <ai.muratova@mail.ru>
2026-07-28 09:50:36 +0500

    Methods implementation add

--- LocatorsTAF.ReqnrollTests/StepDefinitions/GlobalSearchSteps.cs
+++ LocatorsTAF.ReqnrollTests/StepDefinitions/GlobalSearchSteps.cs
@@ -1,33 +1,42 @@
-using System;
+using LocatorsTAF.BusinessLayer.Pages;
+using OpenQA.Selenium;
 using Reqnroll;
+using System;
 
 namespace LocatorsTAF.ReqnrollTests.StepDefinitions
 {
     [Binding]
-    public class GlobalSearchSteps
+    public class GlobalSearchSteps(DriverContext driverContext)
     {
+        private readonly MainPage _mainPage = new MainPage(driverContext.DriverWrapper);
+        private SearchResultPage _searchResultPage;
+        private IWebElement[] _resultLinks;
+
         [Given("I am on the EPAM home page")]
         public void GivenIAmOnTheEPAMHomePage()
         {
-            throw new PendingStepException();
+            _mainPage.AcceptCookiesIfDisplayed();
         }
 
         [When("I search for {string}")]
-        public void WhenISearchFor(string bLOCKCHAIN)
+        public void WhenISearchFor(string searchText)
         {
-            throw new PendingStepException();
+            _searchResultPage = _mainPage.PerformGlobalSearch(searchText);
         }
 
         [Then("every search result should contain {string}")]
-        public void ThenEverySearchResultShouldContain(string bLOCKCHAIN)
+        public void ThenEverySearchResultShouldContain(string searchText)
         {
-            throw new PendingStepException();
+            _resultLinks = _searchResultPage.GetResultLinks();
+            Assert.That(_resultLinks.All(link => link.Text.ToLower().Contains(searchText.ToLower())),
+                $"Not all search results contain the search text '{searchText}'.");
+
         }
 
         [Then("the search results should not be empty")]
         public void ThenTheSearchResultsShouldNotBeEmpty()
         {
-            throw new PendingStepException();
+            Assert.That(_resultLinks, Is.Not.Empty, "No search results were found.");
         }
 
     }
